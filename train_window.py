d34ada68d36a10c4049e1846add0645ac363724f
liukuangxiangzi <daisy.liu.epxmu@gmail.com>
2021-08-17 17:33:31 +0200

    test

--- train_window.py
+++ train_window.py
@@ -18,7 +18,7 @@ from keras.utils import plot_model, Sequence
 from tqdm import tqdm
 import argparse
 
-
+print('test')
 #python train.py -x path-of-audio-feature-file-for-training, -X path-of-audio-feature-file-for-testing, -y path-of-phoneme-file-for-training -Y path-of-phoneme-file-for-testing -o path-of-saving-model
 #e.g. python train.py -x data/audio_feature_train.npy -X data/audio_feature_test.npy -y data/train_phoneme_label_13.npy -Y data/test_phoneme_label_13.npy -o model/audio2pho_model_mfa13label_ep300_1e-4_32.h5
 
